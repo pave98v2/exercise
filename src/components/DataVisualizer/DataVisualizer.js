95405a99f3fdfaa19e5b1e8ba7449b62f6f06ae3
Paavo Tuomensalo-Porkka <paavo.tuomensaloporkka@gmail.com>
2021-09-19 10:57:22 +0300

    Added css, fixed chart indexing
    Added styles to charts.
    Fixed charts x axis index starting from 0.

--- src/components/DataVisualizer/DataVisualizer.js
+++ src/components/DataVisualizer/DataVisualizer.js
@@ -11,6 +11,10 @@ class DataVisualizer extends React.Component {
 
     this.parseData = this.parseData.bind(this);
     this.createGraphs = this.createGraphs.bind(this);
+    this.countMin = this.countMin.bind(this);
+    this.countMax = this.countMax.bind(this);
+    this.countAverage = this.countAverage.bind(this);
+    this.countStandardDeviation = this.countStandardDeviation.bind(this);
   }
 
   componentDidMount() {
@@ -59,42 +63,100 @@ class DataVisualizer extends React.Component {
   createGraphs() {
     let data = Object.entries(this.state.data);
     data.forEach((element, index) => {
+      let yValues = [];
+      let xValues = [];
+      Object.entries(element[1]).forEach((element, index) => {
+        yValues[index] = parseInt(element[0]) + 1;
+        xValues[index] = element[1];
+      });
+      console.log(yValues);
+      console.log(xValues);
       var ctx = document.getElementById("canvas-" + index).getContext('2d');
       var graph = new Chart(ctx, {
         type: 'bar',
         data: {
-          labels: ['Dataset ' + (index+1)],
+          labels: yValues,
           datasets: [{
             label: 'Analog value',
-            data: element[1],
+            data: xValues,
             backgroundColor: 'rgba(255, 99, 132, 0.2)',
             borderColor: 'rgba(255, 159, 64, 1)',
             borderWidth: 1
           }]
         },
         options: {
+          data: {
+            index: index + 1,
+          },
           scales: {
             y: {
-              beginAtZero: false
+              beginAtZero: false,
+              max: 100,
             }
           }
         }
       });
+      console.log(Object.keys(element[1]))
     })
-
   }
+
+  countMin(data) {
+    data = Object.values(data);
+    let length = data.length, min = Infinity;
+    while (length--) {
+      if (data[length] < min) {
+        min = data[length];
+      }
+    }
+    return min;
+  }
+
+  countMax(data) {
+    data = Object.values(data);
+    let len = data.length, max = -Infinity;
+    while (len--) {
+      if (data[len] > max) {
+        max = data[len];
+      }
+    }
+    return max;
+  }
+
+  countAverage(data) {
+    data = Object.values(data);
+    return data.reduce((p, c, i) => { return p + (c - p) / (i + 1) }, 0);
+  }
+
+  countStandardDeviation(data) {
+    data = Object.values(data);
+    const n = data.length
+    const mean = data.reduce((a, b) => a + b) / n
+    return Math.sqrt(data.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n)
+  }
+
 
   render() {
     let data = Object.entries(this.state.data);
     let graphs = data.map((element, index) => {
-      return <canvas id={"canvas-" + index} height="500" width="600"></canvas>;
+      console.log(element);
+      return <div className="chart-container" >
+        <canvas id={"canvas-" + index}></canvas>
+        <div className="numbers-row">
+          <p>Min: <br />{this.countMin(element[1])}</p>
+          <p>Max: <br />{this.countMax(element[1])}</p>
+          <p>Average: <br />{this.countAverage(element[1])}</p>
+          <p>Standard deviation: <br /> {this.countStandardDeviation(element[1])}</p>
+        </div>
+      </div>;
     });
-    console.log(typeof (this.state.data));
 
     return (
       <div className="data-visualizer" >
         <h1>Data Visualizer</h1>
-        {graphs}
+        <div className="graphs-container" >
+          {graphs}
+
+        </div>
       </div>
     );
   }
